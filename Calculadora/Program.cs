ee7c1826029f121ed516e85ae93085e90516e580
AlvarezJeremias <jeremiasal0209@gmail.com>
2022-05-31 11:30:02 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,62 @@
+﻿using Calculadora;
+
+public class PrimerEjercicio
+{
+    public static void Main ()
+    {
+        int operacion;
+        char opcion='y';
+        double resultado;
+        calcu calculo = new calcu();
+        do
+        {
+            do
+            {
+                Console.WriteLine("Seleccione un tipo de operacion: ");
+                Console.WriteLine("1) SUMA");
+                Console.WriteLine("2) RESTA");
+                Console.WriteLine("3) MULTIPLICACION");
+                Console.WriteLine("4) DIVISION");
+                operacion = Convert.ToInt32(Console.ReadLine());
+            } while (operacion < 0 && operacion > 5);
+
+            Console.Write("Escriba el primer numero ");
+            calculo.firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Escriba el segundo numero: ");
+            calculo.secondNumber= Convert.ToDouble(Console.ReadLine());
+
+            switch (operacion)
+            {
+                case 1:
+                    resultado = calculo.suma();
+                    Console.WriteLine($"El resultado es: {resultado}");
+                    break;
+                case 2:
+                    resultado = calculo.resta();
+                    Console.WriteLine($"El resultado es: {resultado}");
+                    break;
+                case 3:
+                    resultado = calculo.producto();
+                    Console.WriteLine($"El resultado es:  {resultado}");
+                    break;
+                case 4: 
+                    if (calculo.secondNumber!=0)
+                    {
+                        resultado = calculo.division();
+                        Console.WriteLine($"El resultado es: {resultado}");
+                        
+                    }else
+                    {
+                        resultado = 0;
+                        Console.WriteLine("No se puede dividir en 0");
+                    }
+                    break;
+            }
+            calculo.firstNumber = calculo.Limpiar();
+            calculo.secondNumber = calculo.Limpiar();
+            resultado = calculo.Limpiar();
+            Console.WriteLine("Desea seguir usando la calculadora? [Y/N]");
+            opcion = Convert.ToChar(Console.ReadLine());
+        } while (opcion== 'Y' || opcion== 'y');
+    }
+}
---
+++ Calculadora/calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class calcu
+    {
+        private double primerNumero;
+        private double segundoNumero;
+
+        public double firstNumber { 
+            get => primerNumero; 
+            set => primerNumero=value; }
+
+        public double secondNumber
+        {
+            get => segundoNumero;
+            set => segundoNumero = value;
+        }
+
+        public double suma ()
+        {
+            return firstNumber+secondNumber;
+        }
+        public double resta()
+        {
+            return firstNumber - secondNumber;
+        }
+        public double producto ()
+        {
+            return firstNumber * secondNumber;
+        }
+        public double division ()
+        {
+            if (secondNumber != 0)
+            {
+                return firstNumber/ secondNumber;
+            }else
+            {
+                return 0;
+            }
+        }
+        public int Limpiar ()
+        {
+            return 0;
+        }
+    }
+}
